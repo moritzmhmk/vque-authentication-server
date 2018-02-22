b392ecfefb9a2dd51e86912ec2b989d21cba9b4f
studhilfa <studhilfa@gmail.com>
2018-02-22 16:42:43 +0100

    refactoring

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Moritz Kanemeier',
     author_email='kanemeier@uni-muenster.de',
     url='https://github.com/moritzmhmk/vque-authentication-server',
-    py_modules=['server'],
+    py_modules=['vque_authentication_server'],
     install_requires=[
         'PyJWT>=1.5.3',
         'cryptography>=2.1.4',
