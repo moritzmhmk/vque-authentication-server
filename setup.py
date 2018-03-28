e75334c3a4312c96871e59b54d130b1b3de95fb9
moritzmhmk <moritzmhmk@gmail.com>
2018-03-28 19:47:58 +0200

    refactor setup, increase version

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
+
+version = '0.0.2'
+url = 'https://github.com/moritzmhmk/vque-authentication-server'
+# download_url = '{}/archive/v{}.tar.gz'.format(url, version)
+
 setup(
-    name='vque_authentication_server',
-    version='0.0.1',
+    name='vque-authentication-server',
+    py_modules=['vque_authentication_server'],
+    version=version,
     description='authentication server for the virtual queueing system',
     author='Moritz Kanemeier',
     author_email='kanemeier@uni-muenster.de',
-    url='https://github.com/moritzmhmk/vque-authentication-server',
-    py_modules=['vque_authentication_server'],
+    url=url,
     install_requires=[
         'PyJWT>=1.5.3',
         'cryptography>=2.1.4',
